1f0e661b20b9c0d261411319b46aeb7b0cd01fef
Folombas <folombas@yandex.ru>
2025-07-04 23:17:29 +0300

    variadic functions

--- project/data_types/functions/functions.go
+++ project/data_types/functions/functions.go
@@ -14,14 +14,36 @@ func multIn(a, b int, c int) int {
 
 // Именованный результат
 func namedReturn() (out int) {
-	out = 3
+	out = 2
 	return
 }
 
 // Несколько результатов
-func multiplrRerutn(in int) (int, error) {
+func multipleReturn(in int) (int, error) {
 	if in > 2 {
 		return 0, fmt.Errorf("some error happend")
 	}
 	return in, nil
+}
+
+// несколько именованных результатов
+func multipleNamedReturn(ok bool) (rez int, err error) {
+	rez = 1
+	if ok {
+		err = fmt.Errorf("some error happend")
+		// аналогично return rez, err
+		// или return 1, fmt.Errorf("some error happend")
+		return
+	}
+	rez = 2
+	return
+}
+
+// не фиксированное количество  параметров
+func sum(in ...int) (result int) {
+	fmt.Printf("in := %#v \n", in)
+	for _, val := range in {
+		result += val
+	}
+	return
 }
