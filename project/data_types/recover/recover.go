e76a3f28965de76625bd1a13beac39fe3f66c72b
Folombas <folombas@yandex.ru>
2025-07-08 11:29:25 +0300

    recover

--- project/data_types/recover/recover.go
+++ project/data_types/recover/recover.go
@@ -1,3 +1,19 @@
 package recover
 
-import
+import "fmt"
+
+
+
+func DeferTest() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("panic happend", err)
+		}
+	}()
+	fmt.Println("Some userful work")
+	panic("something bad happened")
+	}
+
+func main() {
+	DeferTest()
+}
