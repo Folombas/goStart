467b74687f909847402f5ba005882dcecaca9b0b
Folombas <folombas@yandex.ru>
2025-07-10 09:24:43 +0300

    method in slices

--- project/data_types/methods/methods.go
+++ project/data_types/methods/methods.go
@@ -1,6 +1,55 @@
 package methods
 
+import "fmt"
+
 type Person struct {
-	Id 		int
-	Name 	string
+	Id   int
+	Name string
+}
+
+// Не изменит оригинальной структуры, для которой вызван
+func (p Person) UpdateName(name string) {
+	p.Name = name
+}
+
+// Изменяет оригинальную структуру
+func (p *Person) SetName(name string) {
+	p.Name = name
+}
+
+type Account struct {
+	Id   int
+	Name string
+	Person
+}
+
+func (p *Account) SetName(name string) {
+	p.Name = name
+}
+
+type MySlice []int
+
+func (sl *MySlice) Add(val int) {
+	*sl = append(*sl, val)
+}
+
+func DemoMethods() {
+	pers := Person{1, "Gosha"}
+	pers.SetName("Gosha Golang")
+	// (&pers).SetName("Gosha Golang")
+	fmt.Printf("updated person: %#v\n", pers)
+
+	var acc Account = Account{
+		Id:   1,
+		Name: "Gosha",
+		Person: Person{
+			Id:   2,
+			Name: "Gosha Golang",
+		},
+	}
+
+	acc.SetName("golang.gosha")
+	acc.Person.SetName("Test")
+
+	fmt.Printf("%#v\n", acc)
 }
